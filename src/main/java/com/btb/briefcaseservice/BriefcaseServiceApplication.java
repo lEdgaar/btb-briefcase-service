cc4360f1d0bde53e652176ed96772593268dd81e
Edgar Doutón <38222402+lEdgaar@users.noreply.github.com>
2023-12-07 17:16:06 +0100

    Spring Initializr

---
+++ src/main/java/com/btb/briefcaseservice/BriefcaseServiceApplication.java
@@ -0,0 +1,13 @@
+package com.btb.briefcaseservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BriefcaseServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BriefcaseServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/btb/briefcaseservice/BriefcaseServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.btb.briefcaseservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BriefcaseServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
